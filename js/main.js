691be22d897b81ec22ee159e52d31b9237b03225
karetael <karen.taelman@student.arteveldehs.be>
2017-04-01 18:15:39 +0200

    push

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 ;(function() {
-  alert('Welcome to the GDM Web Template developed by drdynscript!');
+  alert('Welcom to Karen Taelman s website!');
 })();
+
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
